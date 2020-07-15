e2a99bcfc1af4ac84571886a76321fca03fce397
DominicPD <dominic.percival@gmail.com>
2020-07-15 12:12:52 -0400

    chore: Setup prettier, ESLint, .gitignore
    Setup prettier to format code without bikeshedding
    Setup ESLint to enforce regular best pratices for JS Code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 const Pet = (props) => {
-    return React.createElement("div", {}, 
-    [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed)
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
 };
-
 
 const App = () => {
-    return React.createElement("div", {},
-    [
-        React.createElement("h1", {}, "Adopt Me"),
-        React.createElement(Pet, {"name": "Stu", "animal": "cat", "breed": "stray"}),
-        React.createElement(Pet, {"name": "Frank", "animal": "Elephant", "breed":"Asian"}),
-        React.createElement(Pet, {"name": "Doris", "animal": "bird", "breed":"parrot"})
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, { name: "Stu", animal: "cat", breed: "stray" }),
+    React.createElement(Pet, {
+      name: "Frank",
+      animal: "Elephant",
+      breed: "Asian",
+    }),
+    React.createElement(Pet, {
+      name: "Doris",
+      animal: "bird",
+      breed: "parrot",
+    }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
